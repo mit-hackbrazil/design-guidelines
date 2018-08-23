3fea01ae528c4e803e6b76d4ea3127a8d3ebb73f
Lucas Cassiano <cassiano@Lucass-MacBook-Pro-2.local>
2018-08-22 22:36:33 -0400

    more components
    TaskViewer
    TeamHeader
    Calendar

--- components/src/App.js
+++ components/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 /*custom components*/
 import TasksList from "./components/TasksList";
+import TeamHeader from "./components/TeamHeader";
+import Calendar from "./components/Calendar";
 
 const defaultTasks = [
   {
@@ -24,15 +26,59 @@ const defaultTasks = [
     done: false,
     start: "10/10/2018",
     end: "12/10/2018"
+  },
+  {
+    name: "tarefa 3",
+    active: false,
+    done: false,
+    start: "10/10/2018",
+    end: "12/10/2018"
+  },
+  {
+    name: "tarefa 3",
+    active: false,
+    done: false,
+    start: "10/10/2018",
+    end: "12/10/2018"
   }
   /*..*/
+];
+
+let team = {
+  name: "NASA",
+  logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/1200px-NASA_logo.svg.png",
+  description: "Exploring the Space and Hiring the best MIT undergrads",
+  link: 'http://nasa.gov'
+}
+
+const events = [
+  {
+    start: '2018-08-20',
+    end: '2018-09-02',
+    eventClasses: 'optionalEvent',
+    title: 'test event',
+    description: 'This is a test description of an event',
+  },
+  {
+    start: '2018-08-19',
+    end: '2018-08-25',
+    title: 'test event',
+    description: 'This is a test description of an event',
+    data: 'you can add what ever random data you may want to use later',
+  },
 ];
 
 class App extends Component {
   render() {
     return (
       <div>
+
+        <TeamHeader team={team} />
         <TasksList tasks={defaultTasks} />
+        <h2>CALENDAR</h2>
+        <Calendar events={events} />
+
+        <h2>POSTS</h2>
       </div>
     );
   }
--- components/src/components/TasksList/index.js
+++ components/src/components/TasksList/index.js
@@ -13,32 +13,57 @@ example of props
 */
 import React, { Component } from "react";
 import { } from "./tasks-lists.css";
-
-//images
-import task_default from "./task-default.svg";
-import task_open from "./task-open.svg";
-import task_ready from "./task-ready.svg";
+import { TaskOpen, TaskDefault, TaskReady } from "./icons";
+import moment from "moment";
+moment.locale('pt-BR');
 
 class Task extends Component {
+
+    updateDimensions = () => {
+        this.setState({ width: window.innerWidth, height: window.innerHeight });
+    }
+
+    componentWillMount = () => {
+        this.updateDimensions();
+    }
+
+    componentDidMount = () => {
+        window.addEventListener("resize", this.updateDimensions);
+    }
+
+    componentWillUnmount = () => {
+        window.removeEventListener("resize", this.updateDimensions);
+    }
+
     render() {
         let { name, active, done, start, end } = this.props.task;
-        let imageSrc;
+        let icon = <TaskDefault />;
 
-        if (done)
-            imageSrc = task_ready;
+        if (active && done)
+            icon = <TaskReady />
         else if (active && !done)
-            imageSrc = task_open;
-        else
-            imageSrc = task_default;
+            icon = <TaskOpen />
+        //converting the dates to readable with moment
+
+
+        let startDate = moment(start).format('MMM D');
+        let endDate = moment(end).format('MMM D');
+
+        let dates = <div className="task-dates">{startDate} - {endDate}</div>;
+        let nameLabel = <div>{name}</div>;
+
+        if (this.state.width < 700) {
+            nameLabel = <div>{this.props.index}</div>;
+            endDate = moment(end).format('D MMM');
+            dates = <div className="task-dates">{endDate}</div>;
+        }
 
         return (<div className="task-item" >
-            <div>{name}</div>
-
-            <div className="task-circle">
-                <img src={imageSrc} />
+            {nameLabel}
+            <div className="task-circle-container">
+                {icon}
             </div>
-
-            <div className="task-dates">{start}<br />{end}</div>
+            {dates}
         </div >);
     }
 }
@@ -50,10 +75,17 @@ class TasksList extends Component {
         //convert all tasks
         let { tasks } = this.props;
         let tasksList = tasks.map((task, index) => {
-            return <Task key={"task-" + index} task={task} />;
+            return <Task key={"task-" + index} task={task} index={index} />;
         })
 
-        return <div className="task-list">{tasksList}</div>
+        return <div className="task-list-container">
+            <div className="task-line-container" >
+                <div className="task-line" />
+            </div>
+            <div className="task-list">
+                {tasksList}
+            </div>
+        </div>
     }
 }
 
